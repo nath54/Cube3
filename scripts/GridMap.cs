cadc34f52887167d5bf6984e23c7a0db94f8bd71
nath54 <=>
2020-07-09 11:53:04 +0200

    aa

--- scripts/GridMap.cs
+++ scripts/GridMap.cs
@@ -5,20 +5,50 @@ public class GridMap : Godot.GridMap
 {
     OpenSimplexNoise noise = new OpenSimplexNoise();
 
+    public string tipe="platforms";
+    public int tx = 32;
+    public int tz = 32;
+    public int ty = 32;
+    public int nbchem=15;
+    public int depx=1;
+    public int depz=1;
+    public int depy=1;
     public void generatePlatforms(){
 
     }
 
-    public void generateMaze(){
-
+    public void generateMaze2D(){
+        //creation du sol
+        for(int x=0; x<tx; x++){
+            for(int z=0; z<tx; z++){
+                SetCellItem(x,0,z,0);
+            }
+        }
+        //creation des murs
+        for(int x=0; x<tx; x++){
+            for(int z=0; z<tx; z++){
+                SetCellItem(x,1,z,1);
+            }
+        }
+        //on remove les murs
+        Random rand = new Random();
+        depx=rand.Next(1,tx-1);
+        depz=rand.Next(1,ty-1);
+        int ax=depx;
+        int az=depz;
+        for(int w=0; w<nbchem; w++){
+            SetCellItem(ax,1,az, -1);
+            ax+=rand.Next(-1,2);
+            ay+=rand.Next(-1,2);
+        }
     }
 
-    public void generate(string tipe){
+    public void generate(){
         if(tipe=="platforms"){
             generatePlatforms();
         }
         else if(tipe=="maze"){
-            generateMaze();
+            generateMaze2D();
         }
     }
 
--- scripts/World.cs
+++ scripts/World.cs
@@ -15,151 +15,8 @@ public class World : Spatial
     public float timeLeft; //In seconds
     public float timeTotal; //In seconds
     public Timer timer;
-
-    public void GeneratePlatformMethod(){
-        //Base coordinates
-        int xx = 0,
-            yy = 0,
-            zz = 0;
-        int tx = 3,
-            ty = 2,
-            tz = 3;
-        //Create the Base Materials
-        SpatialMaterial groundMat = new SpatialMaterial();
-        groundMat.AlbedoColor = new Godot.Color(0.8F,0F,0F);
-        groundMat.Metallic=1;       
-        SpatialMaterial finMat = new SpatialMaterial();
-        finMat.AlbedoColor = new Godot.Color(0.0F,0.5F,0F);
-
-        List<MeshInstance> liste = new List<MeshInstance> {};
-        
-        //Create the blocks
-        int nbblocks=10;
-        int max_security=50;
-        int security=0;
-        for(int w=0; w<=nbblocks; w++){
-            if(security==max_security){
-                break;
-            }
-            //The Mesh : 
-            MeshInstance mesh = new MeshInstance();
-            mesh.Name="platform_"+Convert.ToString(w);
-            mesh.Mesh = new CubeMesh();
-            mesh.Scale = new Vector3(tx,ty,tz);
-            mesh.Translation = new Vector3(xx,yy,zz);
-            //Add the collision
-            mesh.CreateTrimeshCollision();
-            //Add the collision security shape
-            Area ca = new Area();
-            ca.Name="security_zone";
-            CollisionShape cs = new CollisionShape();
-            cs.Shape=new BoxShape();
-            cs.Scale=new Vector3(1,3,1);
-            ca.AddChild(cs);
-            mesh.AddChild(ca);
-            //Add the material
-            mesh.MaterialOverride = groundMat;
-            //add it to the scene
-            AddChild(mesh);
-            liste.Add(mesh);
-            //Change the coordinates for the next one
-            if(w!=nbblocks){
-                bool derange=false;
-                security=0;
-                int dx=0;
-                int dy=0;
-                int dz=0;
-                do{
-                    var rand = new Random();
-                    dx=rand.Next(2,5)*tx;
-                    dz=rand.Next(2,5)*ty;
-                    dy=0;
-                    if(xx!=0 && zz!=0){
-                        dy=rand.Next(5,6);
-                    }
-                    //
-                    if(rand.Next(1,3)==2){ dx*=-1; }
-                    if(rand.Next(1,3)==2){ dy*=-1; }
-                    if(rand.Next(1,3)==2){ dz*=-1; }
-                
-                    //Create The Future Mesh to test
-                    MeshInstance next_mesh = new MeshInstance();
-                    next_mesh.Mesh = new CubeMesh();
-                    next_mesh.Scale = new Vector3(tx,ty,tz);
-                    next_mesh.Translation = new Vector3(xx+dx,yy+dy,zz+dz);
-                    //Add the collision
-                    next_mesh.CreateTrimeshCollision();
-                    AddChild(next_mesh);
-
-                    for(int i=0; i<w; i++){
-                        Area cia=(Area)GetNode("platform_"+Convert.ToString(w)+"/security_zone");
-                        //GD.Print(cia.OverlapsArea(next_mesh));
-                        if(cia.OverlapsArea(next_mesh)){
-                            derange=true;
-                            GD.Print("deranged");
-                        }
-                    }
-
-                    RemoveChild(next_mesh);
-
-                }while(derange && security<max_security);
-                
-                if(!(security==max_security)){
-                    xx+=dx;
-                    yy+=dy;
-                    zz+=dz;
-                    //
-                    if(zz<player.hauteur_min+ty){
-                        zz=Convert.ToInt32(player.hauteur_min)+ty;
-                    }
-                }
-                
-            }
-        }
-        //on donne les coordonnées à la fin du niveau
-        finNiv.Translation = new Vector3(xx , yy+ty+0.3F, zz);
-
-        
-    }
-
-    public void GenerateGridMapMethod(){
-        //
-        SpatialMaterial floorMat = new SpatialMaterial();
-        floorMat.AlbedoColor = new Godot.Color(0.0F,0.3F,0F);
-        //
-        SpatialMaterial wallMat = new SpatialMaterial();
-        wallMat.AlbedoColor = new Godot.Color(0.8F,0F,0F);
-        //
-        GridMap gridMap = new GridMap();
-        Spatial mshlib =new Spatial(); 
-        //
-        MeshInstance floor = new MeshInstance();
-        floor.Mesh=new CubeMesh();
-        floor.CreateTrimeshCollision();
-        floor.MaterialOverride = floorMat;
-        //
-        MeshInstance wall = new MeshInstance();
-        wall.Mesh=new CubeMesh();
-        Area area=new Area();
-        CollisionShape cs = new CollisionShape();
-        area.AddChild(cs);
-        wall.AddChild(area);
-        wall.MaterialOverride = wallMat;
-        //
-        mshlib.AddChild(floor);
-        mshlib.AddChild(wall);
-        
-        //
-        gridMap.MeshLibrary=mshlib;
-
-        AddChild(gridMap);
-    }
-
-    public void mapCreation(){
-        string method="gridmap";
-        if(method == "platforms"){ GeneratePlatformMethod(); }
-        else if(method == "gridmap"){ GenerateGridMapMethod(); }
-    }
+    public GridMap gridMap;
+    public string tipemap = "maze";
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -173,7 +30,12 @@ public class World : Spatial
         finNiv = (Spatial)GetNode("FinNiv");
         finNivArea = (Area)GetNode("FinNiv/Area");
         //
-        mapCreation();
+        gridMap = (GridMap)GetNode("GridMap");
+        gridMap.tipe=tipemap;
+        gridMap.tx=32;
+        gridMap.ty=32;
+        gridMap.tz=32;
+        gridMap.generate();
         //
         loading= (Control)GetNode("Loading");
         loading.Visible=false;
