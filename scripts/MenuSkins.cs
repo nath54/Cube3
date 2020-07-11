535408fe50ae3dcda9bc2eef99b6b16eef7eff5c
nath54 <=>
2020-07-11 13:56:12 +0200

    aa

---
+++ scripts/MenuSkins.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public class MenuSkins : Control
+{
+    
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    
+
+}
