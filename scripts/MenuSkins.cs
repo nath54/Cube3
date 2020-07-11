6787a3d9eae1ba32e211e5ee93c44b40ac142107
nath54 <=>
2020-07-11 15:56:09 +0200

    aa

--- scripts/MenuSkins.cs
+++ scripts/MenuSkins.cs
@@ -19,9 +19,11 @@ public class MenuSkins : Control
     }
 
     public void createSkins(){
-        for(int w=0; w<globale.max_skin; w++){
-            Control skin_s=new Control();
+        for(int w=0; w<=globale.max_skin; w++){
+            Sprite skin_s = new Sprite();
+            skin_s.Texture = ResourceLoader.Load("res://imgs/menu_skin_entity.png") as Texture;
             Godot.Label label = new Godot.Label();
+            label.RectPosition=new Vector2(-18,-65);
             label.Text = "Skin n°"+w;
             skin_s.AddChild(label);
             ButtonSkinSelector bt = new ButtonSkinSelector();
@@ -35,7 +37,10 @@ public class MenuSkins : Control
                 bt.Disabled=false;
             }
             bt.Connect("clique", this, nameof(selectSkin));
+            bt.RectPosition=new Vector2(-28,50);
             skin_s.AddChild(bt);
+            skin_s.Scale=new Vector2(2,2);
+            skin_s.Position=new Vector2(100+w*110*skin_s.Scale.x,200);
             container.AddChild(skin_s);
         }
     }
