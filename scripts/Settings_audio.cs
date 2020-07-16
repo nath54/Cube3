a954d35170e199358675d38368d6ce0dd029551e
nath54 <=>
2020-07-16 13:51:13 +0200

    aa

---
+++ scripts/Settings_audio.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public class Settings_audio : Control
+{
+    private Button bt_s_game;
+    private Button bt_s_graphics;
+    private Button bt_s_audio;
+    private Button bt_s_other;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        bt_s_game = (Button)GetNode("Menu_Buttons/HScrollBar/Bt_game");
+        bt_s_graphics = (Button)GetNode("Menu_Buttons/HScrollBar/Bt_graphics");
+        bt_s_audio = (Button)GetNode("Menu_Buttons/HScrollBar/Bt_audio");
+        bt_s_other = (Button)GetNode("Menu_Buttons/HScrollBar/Bt_other");
+    }
+
+    public void _on_Bt_game_pressed(){ GetTree().ChangeScene("res://menus/Settings_game.tscn"); }
+    public void _on_Bt_graphics_pressed(){ GetTree().ChangeScene("res://menus/Settings_graphics.tscn"); }
+    public void _on_Bt_audio_pressed(){ GetTree().ChangeScene("res://menus/Settings_audio.tscn"); }
+    public void _on_Bt_other_pressed(){ GetTree().ChangeScene("res://menus/Settings_other.tscn"); }
+    
+}
