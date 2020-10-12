0310273f107a885937c0ca7bff1abf871e8272fc
nath54 <cerisara.nathan@gmail.com>
2020-10-12 16:56:22 +0200

    aa

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -7,7 +7,6 @@ public class MainMenu : Control
     private TextureButton bt_start_adventure;
     private TextureButton bt_settings;
     private TextureButton bt_quit;
-    private Control loading;
     public Global globale;
 
     // Called when the node enters the scene tree for the first time.
@@ -20,13 +19,10 @@ public class MainMenu : Control
         bt_start_adventure = (TextureButton)GetNode("Bt_play_adventure");
         bt_settings = (TextureButton)GetNode("Bt_settings");
         bt_quit = (TextureButton)GetNode("Bt_quit");
-        loading = (Control)GetNode("Loading");
-        loading.Visible=false;
 
     }
 
     public void _on_Bt_play_arcade_pressed(){
-        loading.Visible=true;
         globale.level=1;
         GetTree().ChangeScene("res://menus/PreArcadeLevel.tscn");
     }
